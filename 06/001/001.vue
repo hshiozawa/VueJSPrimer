a20e324ae9e439e61f02113402b4b351384b1931
Hajime Shiozawa <hajime.shiozawa@gmail.com>
2020-10-01 15:39:19 +0900

    added chapter 06

---
+++ 06/001/001.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="message">メッセージ: {{ msg }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {msg: 'こんにちは!'}
+  }
+}
+</script>
+
+<style>
+.message {
+  color: #42b938;
+}
+</style>
---
+++ 06/002/foo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="foo">
+    <h1 class="header">Foo コンポーネント</h1>
+    <p>これは Foo コンポーネントです</p>
+  </div>
+</template>
+
+<style>
+p {
+  text-decoration: underline;
+}
+</style>
+
+<style scoped>
+.foo {
+  border: solid 1px green;
+  margin: 4px;
+  padding: 4px;
+}
+</style>
